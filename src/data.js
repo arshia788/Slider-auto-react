39adaf436bde50dee9216a8cac6ab49d8fe3389b
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-12 23:54:25 +0330

    first side of project

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const people = [
+     {
+         id: 1,
+         image: './images/1.jpg',
+         name: 'sara amani',
+         title:  'project manager',
+         qoute: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque In egestas erat imperdiet sed euismod nisi porta lorem mollis Morbi tristique senectus et netus.'
+     },
+      {
+         id: 2,
+         image: './images/2.jpg',
+         name: 'alex fire',
+         title: 'web designer',
+         
+         qoute: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque In egestas erat imperdiet sed euismod nisi porta lorem mollis Morbi tristique senectus et netus.'
+     },
+     {
+         id: 3,
+         image: './images/3.jpg',
+         name: 'ronalo messi',
+         title: 'house wife',
+         
+         qoute: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque In egestas erat imperdiet sed euismod nisi porta lorem mollis Morbi tristique senectus et netus.'
+     }
+ ]
+ 
+ export default people;
